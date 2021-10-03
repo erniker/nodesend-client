18454ef9f7058b1df0a7f663d2b4ea63532cf644
erniker <josepablomedinagrande@hotmail.com>
2021-10-03 18:37:51 +0200

    +

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     // backendURL: 'http://localhost:4000',
     // frontendURL: 'http://localhost:3000',
-    backendURL: 'https://peaceful-reef-68464.herokuapp.com/',
-    frontendURL: 'https://nodesend-client-2uyxvngbl-erniker.vercel.app/',
+    backendURL: 'https://blooming-woodland-88391.herokuapp.com',
+    frontendURL: 'https://nodesend-client-alpha.vercel.app',
   },
 }
